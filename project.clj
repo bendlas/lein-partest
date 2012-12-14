841f579b19edf7f46e38926af37447a0940b0daf
Herwig Hochleitner <hhochleitner@gmail.com>
2012-12-14 20:42:25 +0100

    Mention github page as url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject net.bendlas/lein-partest "0.1.0"
   :description "Benchmark harness for parallelism"
+  :url "https://github.com/bendlas/lein-partest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
